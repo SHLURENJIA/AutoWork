da9f4228a4d8f10ab96a54e737c217e9825e9cfc
SHLURENJIA <SH_LURENJIA@163.com>
2022-02-23 11:01:28 +0800

    change script

--- glados_sign/auto_sign_glados.sh
+++ glados_sign/auto_sign_glados.sh
@@ -4,15 +4,16 @@
 
 #设置第一个参数为cookie
 cookie=$1
-user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"
-content_type="application/json;charset=UTF-8"
-accept="application/json, text/plain, */*"
-accept_encoding="gzip, deflate, br"
 
 url="https://glados.rocks/api/user/checkin"
+RESULT=$(curl -H "cookie:${cookie}"\
+ -H "Content-Type: application/json"\
+ -d "{\"token\": \"glados_network\"}"\
+ -X POST $url)
 
-# >> 是追加写入
-curl -i -o >> ./auto_sign_glados_log.txt -X POST -H "accept:${accept}" -H "accept_encoding:${accept_encoding}" -H "content_type:${content_type}" -H "user_agent:${user_agent}" -H "cookie:${cookie}" $url 
+echo -e $RESULT >> ./auto_sign_glados_log.txt
 
 #-e打开转义字符。-E关闭，默认关闭
 echo -e "\n--------------------------------\n" >> ./auto_sign_glados_log.txt
+
+echo $RESULT
