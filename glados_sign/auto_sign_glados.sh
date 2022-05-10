acba37d7df670a0af302641ace6a9ae0bdc382c4
SHLURENJIA <SH_LURENJIA@163.com>
2022-05-10 20:28:08 +0800

    Update auto_sign_glados.sh
    fix token error

--- glados_sign/auto_sign_glados.sh
+++ glados_sign/auto_sign_glados.sh
@@ -4,11 +4,12 @@
 
 #设置第一个参数为cookie
 cookie=$1
+params=$2
 
 url="https://glados.rocks/api/user/checkin"
 RESULT=$(curl -H "cookie:${cookie}"\
  -H "Content-Type: application/json"\
- -d "{\"token\": \"glados_network\"}"\
+ -d "${params}"\
  -X POST $url)
 
 echo -e $RESULT >> ./auto_sign_glados_log.txt
