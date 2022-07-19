ea62b5fd3dad29b16d96c4a7f60e707a92e8388c
SHLURENJIA <SH_LURENJIA@163.com>
2022-07-19 11:30:06 +0800

    Update auto_sign_glados.sh

--- glados_sign/auto_sign_glados.sh
+++ glados_sign/auto_sign_glados.sh
@@ -3,8 +3,7 @@
 # $1为第一个参数，$0为文件名
 
 #设置第一个参数为cookie
-cookie=$1
-params=$2
+cookie="_ga=GA1.2.1891431373.1644477268; koa:sess=eyJ1c2VySWQiOjEzMzU0MiwiX2V4cGlyZSI6MTY3MDk4MTEzNzM3MCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=fK5IIA8XhjG9Qf-RuCaK-iJQ4BA; _gid=GA1.2.833999213.1656741852; __cf_bm=17KdVIzKhZBzujCaA0fUJnSCeEREpWgJnT3QYmUtgRQ-1656741852-0-AR4rOZ/4+0stB5mrbECmX9Bhz56QP4qGNitVetx6/DdzA5sjgj+g4LzzLVNKtuCU59fqZNg++7NccVbLmPUqmkG61HkbSn7vVxyXen5O+VtlqlTh6R2NUG/1ZEnqhpxuNA=="
 
 url="https://glados.rocks/api/user/checkin"
 RESULT=$(curl -H "cookie:${cookie}"\
