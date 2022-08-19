c5185208f5a8f28f52114d21d39f01e10cf32af0
SHLURENJIA <SH_LURENJIA@163.com>
2022-08-19 18:09:55 +0800

    Update auto_sign_glados.sh

--- glados_sign/auto_sign_glados.sh
+++ glados_sign/auto_sign_glados.sh
@@ -3,11 +3,14 @@
 # $1为第一个参数，$0为文件名
 
 #设置第一个参数为cookie
-cookie="_ga=GA1.2.1891431373.1644477268; koa:sess=eyJ1c2VySWQiOjEzMzU0MiwiX2V4cGlyZSI6MTY3MDk4MTEzNzM3MCwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=fK5IIA8XhjG9Qf-RuCaK-iJQ4BA; _gid=GA1.2.833999213.1656741852; __cf_bm=17KdVIzKhZBzujCaA0fUJnSCeEREpWgJnT3QYmUtgRQ-1656741852-0-AR4rOZ/4+0stB5mrbECmX9Bhz56QP4qGNitVetx6/DdzA5sjgj+g4LzzLVNKtuCU59fqZNg++7NccVbLmPUqmkG61HkbSn7vVxyXen5O+VtlqlTh6R2NUG/1ZEnqhpxuNA=="
+cookie="_ga=GA1.2.1399640006.1657632891; koa:sess=eyJ1c2VySWQiOjEzMzU0MiwiX2V4cGlyZSI6MTY4MzU1Mjk0NDM1MywiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=xn4Y29-VzTkj4Mi4d_5TEkKdIcs; cf_clearance=uLKtWDIX8Qfgi.9CE0sLl60FT65IIfDU8Zl5bbZ2GCI-1660007870-0-150; _gid=GA1.2.711660218.1660790321; Cookie=enabled; Cookie.sig=lbtpENsrE0x6riM8PFTvoh9nepc; _gat_gtag_UA_104464600_2=1; __cf_bm=7Y_ohnA2tGW.yKqrl7QO__be0aRNgzU4FIuFIMbsXQE-1660903401-0-Ab64K/u8ZaqGAlbREwS9CD1Ldey/YllbUYH/jsdtNjZs92vJjZ2k9NNnE92QugYp79t4L0uYn5OC0VK3B6reYEFFOo050xXw8rf7cvzKVxQ1tHIAzvnsURx79BnGTLX8NQ=="
 
 url="https://glados.rocks/api/user/checkin"
 RESULT=$(curl -H "cookie:${cookie}"\
  -H "Content-Type: application/json"\
+ -H "sec-ch-ua: \"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\""\
+ -H "sec-ch-ua-platform: \"macOS\""\
+ -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"\
  -d "{\"token\": \"glados.network\"}"\
  -X POST $url)
 
